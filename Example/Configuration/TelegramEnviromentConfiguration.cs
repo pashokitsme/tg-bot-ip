614c609bb8742dfead5680cf2f372a4ae34d5b8c
pashamaladec <pashamail79@gmail.com>
2022-02-26 22:55:01 +0300

    fix env config

--- Example/Configuration/TelegramEnviromentConfiguration.cs
+++ Example/Configuration/TelegramEnviromentConfiguration.cs
@@ -4,7 +4,13 @@ public class TelegramEnviromentConfiguration : ITelegramBotConfiguration
 {
 	public string Token { get; } = Environment.GetEnvironmentVariable("token");
 	public string Webhook { get; } = Environment.GetEnvironmentVariable("webhook");
-	public string ListeningUrl { get; } = Environment.GetEnvironmentVariable("listening-url");
+	public string ListeningUrl { get; }
 	public string Route { get; } = Environment.GetEnvironmentVariable("route");
 	public string OpenWeatherToken { get; } = Environment.GetEnvironmentVariable("openweather-token");
+	
+	public TelegramEnviromentConfiguration()
+	{
+		ListeningUrl = string.Format(Environment.GetEnvironmentVariable("listening-url") ?? "http://+:{0}/", Environment.GetEnvironmentVariable("PORT"));
+		
+	}
 }
